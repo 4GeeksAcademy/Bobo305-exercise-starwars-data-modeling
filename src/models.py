2f4e3a2f0f0e99c57a34af71d8b348a49c6d743e
bobo305 <126503694+bobo305@users.noreply.github.com>
2023-08-16 12:13:17 +0000

    done

--- src/models.py
+++ src/models.py
@@ -4,29 +4,65 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template, request, redirect, url_for
 
-Base = declarative_base()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+           'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username=db.Column(db.String(250), unique = True, nullable = False )
+    password = db.Column(db.String(250), nullable = False)
+    favorite_Character = db.Column(db.String(250))
+    favorite_Planets = db.Column(db.String(250))
+
+
+class Character(db.Model):
+    __tablename__ = 'Character'
+    id = db.Column(db.Integer, primary_key = True)
+    name =db.Column(db.String(250), ForeignKey('Favorite.Character'), unique = True, nullable = False )
+    birth_year = db.Column(db.Integer, nullable = False)
+    homeworld = db.Column(db.String(250), nullable =False)
+    skin_Color = db.Column(db.String(250), nullable =False )
+    eye_Color = db.Column(db.String(250), nullable =False )
+    mass = db.Column(db.Integer, nullable =False )
+    height = db.Column(db.Integer, nullable =False )
+
+
+class Planets(db.Model):
+    __tablename__ = 'Planets'
+    id = db.Column(db.Integer, primary_key = True)
+    name =db.Column(db.String(250),ForeignKey('Favorite.Planet'), unique = True, nullable = False)
+    Population = db.Column(db.Integer, nullable =False)
+    Terrain = db.Column(db.Integer, nullable =False)
+    Diameter = db.Column(db.Integer, nullable =False) 
+    
+
+class Favorites(db.Model):
+    __tablename__ = 'Favorite'
+    id = db.Column(db.Integer, primary_key = True)
+    Planet =db.Column(db.String(250), ForeignKey('user.favorite_Planets'), unique = True, nullable = False)
+    Character =db.Column(db.String(250), ForeignKey('user.favorite_Character'),unique = True, nullable = False)
+
+
+
+        
+
+
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(db, 'diagram.png')
